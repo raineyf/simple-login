351741d0c1f05183243b457c60da77cca92b16cc
Rainey F <raineyfleming@protonmail.com>
2021-03-18 23:16:51 -0400

    add post functions

--- app.js
+++ app.js
@@ -1,13 +1,58 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const bodyParser = require("body-parser");
 const port = 3000;
 
 app.get("/", function (req, res) {
     res.redirect("/login.html");
 });
 
+app.use(bodyParser.json());
+
 app.use(express.static(path.join(__dirname, "src")));
+
+const users = [
+    {
+        username: "level",
+        password: "Access123",
+    },
+];
+
+app.post("/login", (req, res) => {
+    const user = req.body;
+    let response = {
+        loginStatus: "fail",
+        message: "user does not exist",
+    };
+    users.forEach((u) => {
+        if (u.username == user.username) {
+            if (u.password == user.password) {
+                response = {
+                    loginStatus: "success",
+                    redirect: "/success.html",
+                };
+                return;
+            } else {
+                response = {
+                    loginStatus: "fail",
+                    message: "username and password do not match",
+                };
+                return;
+            }
+        }
+    });
+    res.send(JSON.stringify(response));
+});
+
+app.post("/signup", (req, res) => {
+    const user = req.body;
+    users.push(user);
+    let response = {
+        signupStatus: "success",
+    };
+    res.send(JSON.stringify(response));
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
---
+++ src/index.js
@@ -0,0 +1,94 @@
+(() => {
+    if (!document.querySelector("form")) return;
+    const loginForm = document.querySelector("#login-form");
+    const signupForm = document.querySelector("#signup-form");
+    const usernameInput = document.querySelector("#username");
+    const passwordInput = document.querySelector("#password");
+    const errorMessages = document.querySelectorAll(".input-error");
+    const userError = document.querySelector("#username-error");
+    const passError = document.querySelector("#password-error");
+    const clearErrorMessages = () => {
+        errorMessages.forEach((m) => {
+            m.classList.remove("active");
+        });
+    };
+    const getFormData = () => {
+        let formData = {
+            username: usernameInput.value,
+            password: passwordInput.value,
+        };
+        return formData;
+    };
+    if (loginForm) {
+        const submitForm = (formData) => {
+            clearErrorMessages();
+            const options = {
+                method: "POST",
+                body: JSON.stringify(formData),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            };
+            fetch("/login", options)
+                .then((res) => res.json())
+                .then((json) => {
+                    if (json.loginStatus == "success") {
+                        window.location = json.redirect;
+                    } else if (json.message == "user does not exist") {
+                        userError.classList.add("active");
+                        usernameInput.focus();
+                    } else if (
+                        json.message == "username and password do not match"
+                    ) {
+                        passError.classList.add("active");
+                        passwordInput.focus();
+                    }
+                });
+        };
+        loginForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+            submitForm(getFormData());
+        });
+    }
+    if (signupForm) {
+        const submitForm = (formData) => {
+            clearErrorMessages();
+            const userVal = usernameInput.value;
+            const passVal = passwordInput.value;
+            const hasNumber = (myString) => {
+                return /\d/.test(myString);
+            };
+            if (userVal.length < 5) {
+                userError.classList.add("active");
+                usernameInput.focus();
+            } else if (
+                passVal.toLowerCase() === passVal ||
+                passVal.toUpperCase() === passVal ||
+                !hasNumber(passVal)
+            ) {
+                passError.classList.add("active");
+                passwordInput.focus();
+            } else {
+                const options = {
+                    method: "POST",
+                    body: JSON.stringify(formData),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                };
+                fetch("/signup", options)
+                    .then((res) => res.json())
+                    .then((json) => {
+                        if (json.signupStatus == "success") {
+                            window.alert("Account Added!");
+                        }
+                    });
+            }
+        };
+
+        signupForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+            submitForm(getFormData());
+        });
+    }
+})();
